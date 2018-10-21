f87559b96044c1e1de3728a5e7ccc55ecf722f4a
jdotero <jdotero@rincondelgallego.com>
2018-10-21 23:55:18 +0200

    Test source

---
+++ src/main/java/com/jdotero/gdg/test/Greeting.java
@@ -0,0 +1,20 @@
+package com.jdotero.gdg.test;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
